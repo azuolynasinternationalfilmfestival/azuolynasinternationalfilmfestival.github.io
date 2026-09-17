5dbd8634c92816a6e8d7797c6e254089ef100b24
azuolynasinternationalfilmfestival <azuolynasfilmfestival@gmail.com>
2026-09-17 17:40:55 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAl-aLSlSHUdrZ4Rr4x23n3bu3QFZSYyB0",
+  authDomain: "azuolynas-film-fest.firebaseapp.com",
+  projectId: "azuolynas-film-fest",
+  storageBucket: "azuolynas-film-fest.firebasestorage.app",
+  messagingSenderId: "541713316291",
+  appId: "1:541713316291:web:51de85684512c9d7e6a576",
+  measurementId: "G-9Z050BPHJ5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+const FESTIVAL_LOGO_URL = "https://firebasestorage.googleapis.com/v0/b/azuolynas-film-fest.firebasestorage.app/o/azuolynasfilmfest.webp?alt=media";
